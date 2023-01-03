8de08e7ff53a360c67587efbb7ab7895f34da050
ADMIN <sreeinbox@gmail.com>
2023-01-03 16:01:44 +0530

    shell commands - echo colours

--- 02-Printing.sh
+++ 02-Printing.sh
@@ -1,1 +1,10 @@
 echo '*****Hello Devops*******'
+
+echo -e "\e[31mMessage\e[0m"
+echo -e "\e[32mMessage\e[0m"
+echo -e "\e[33mMessage\e[0m"
+echo -e "\e[34mMessage\e[0m"
+echo -e "\e[35mMessage\e[0m"
+echo -e "\e[36mMessage\e[0m"
+echo -e "\e[31mMessage\e[0m"
+
